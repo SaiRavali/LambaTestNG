e83126248e575d244da69725452db914346b792f
SaiRavali <42229607+SaiRavali@users.noreply.github.com>
2023-02-27 14:58:30 +0530

    Update Scenario2.java

--- Scenario2.java
+++ Scenario2.java
@@ -1,21 +1,15 @@
 package scenarios;
 	
-	package scenarios;
-	
-	import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 	public class Scenario2 extends BaseClass {
-
-		
-
 	
     @Test 
     
     public void checkboxDemo()  {
-
-    	
+ 	
     	driver.findElement(By.linkText("Checkbox Demo")).click();
         
     	WebElement checkBoxSelected = driver.findElement(By.id("isAgeSelected"));
